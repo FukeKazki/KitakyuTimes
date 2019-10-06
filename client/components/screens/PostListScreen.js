ce2dafd9defc13a276054cee1fae8343835914d7
FukeKazki <kazkichi0906@gmail.com>
2019-10-06 15:03:38 +0900

    Add: 各種画面の追加

---
+++ client/components/screens/PostListScreen.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import {
+    Container,
+    Header,
+    Body,
+    Content,
+    Title,
+    Text,
+    Footer,
+    FooterTab,
+    Button,
+    Icon,
+} from 'native-base';
+export default class Map extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        const { navigate } = this.props.navigation
+        return (
+            <Container>
+                <Header>
+                    <Body>
+                        <Title>PostListScreen</Title>
+                    </Body>
+                </Header>
+                <Content>
+                </Content>
+            </Container>
+        )
+    }
+}
