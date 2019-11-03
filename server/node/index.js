98b90bf584ba157fd422ccc11157a4ef92181817
FukeKazki <kazkichi0906@gmail.com>
2019-11-03 10:50:44 +0900

    Fix: ファイル配置の変更 サービスごとにディレクトリを追加し、それぞれファイルを管理する

--- server/node/index.js
+++ server/node/index.js
@@ -1,18 +1,16 @@
+'use strict'
 import express from 'express'
-
-const PORT = 3000
-
 const app = express()
-
-app.use(function (req, res, next) {
+const PORT = 3000
+app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*")
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
     next()
 })
-
-app.get('/', async (req, res) => {
-    res.status(200).send('Hello World').end()
+app.get('/', (req, res) => {
+    res.status(200)
+    res.send('Hello Express')
 })
-
-app.listen(PORT)
-console.log(`Server running at ${PORT}`)
+app.listen(PORT, () => {
+    console.log('listen to: ' + PORT)
+})
