32e70dc7972226be37370f55817a295f86fd70c8
mattsun <nene0819.kmnk@gmail.com>
2019-11-09 13:49:35 +0900

    friendlistを実装しました。

--- client/components/screens/FriendListScreen.js
+++ client/components/screens/FriendListScreen.js
@@ -2,6 +2,11 @@ import React from 'react'
 import {
     Container,
     Content,
+    List,
+    ListItem,
+    Body,
+    Text
+
 } from 'native-base'
 import { DrawerActions } from 'react-navigation-drawer'
 import Header from '../organisms/Header'
@@ -15,6 +20,23 @@ export default class Map extends React.Component {
             <Container>
                 <Header dispatch={() => dispatch(DrawerActions.toggleDrawer())} header_title="FriendListScreen" />
                 <Content>
+                    <List>                    
+                        <ListItem thumbnail>
+                            <Body>
+                                <Text>Aaron Bennet</Text>
+                            </Body>
+                        </ListItem>
+                        <ListItem thumbnail>
+                            <Body>
+                                <Text>Bradley Horowitz</Text>
+                            </Body>
+                        </ListItem>                    
+                        <ListItem thumbnail>
+                            <Body>
+                                <Text>Ali Connors</Text>
+                            </Body>
+                        </ListItem>  
+                    </List>
                 </Content>
             </Container>
         )
