6cf1c4c9cb8e052e5f7af35cdaf7c5a3a5f13cb4
mattsun <nene0819.kmnk@gmail.com>
2019-11-13 20:52:58 +0900

    インデント修正

--- client/components/screens/MapScreen.js
+++ client/components/screens/MapScreen.js
@@ -16,27 +16,26 @@ export default class Map extends React.Component {
         return (
             <Container>
                 <Header dispatch={() => dispatch(DrawerActions.toggleDrawer())} header_title="MapScreen" />
-                    <MapView
-                        style={{flex: 1}}
-                        initialRegion={{
-                            latitude: 37.78825,
-                            longitude: -122.4324,
-                            latitudeDelta: 0.0922,
-                            longitudeDelta: 0.0421
-                        }}
-                    />
-                    <ActionButton 
-                        buttonColor="rgb(255,0,102)">
-                        <ActionButton.Item buttonColor='#5D99FF' title="dog" onPress={() => console.log("notes tapped!")}>
-                            <Icon name="dog" style={styles.actionButtonIcon} />
-                        </ActionButton.Item>
-                        <ActionButton.Item buttonColor='#00CC99' title="cat" onPress={() => {}}>
-                            <Icon name="cat" style={styles.actionButtonIcon} />
-                        </ActionButton.Item>
-                        <ActionButton.Item buttonColor='#FF69A3' title="good" onPress={() => {}}>
-                            <Icon name="heart" style={styles.actionButtonIcon} />
-                        </ActionButton.Item>
-                    </ActionButton>
+                <MapView
+                    style={{flex: 1}}
+                    initialRegion={{
+                        latitude: 37.78825,
+                        longitude: -122.4324,
+                        latitudeDelta: 0.0922,
+                        longitudeDelta: 0.0421
+                    }}
+                />
+                <ActionButton buttonColor="rgb(255,0,102)">
+                    <ActionButton.Item buttonColor='#5D99FF' title="dog" onPress={() => console.log("notes tapped!")}>
+                        <Icon name="dog" style={styles.actionButtonIcon} />
+                    </ActionButton.Item>
+                    <ActionButton.Item buttonColor='#00CC99' title="cat" onPress={() => {}}>
+                        <Icon name="cat" style={styles.actionButtonIcon} />
+                    </ActionButton.Item>
+                    <ActionButton.Item buttonColor='#FF69A3' title="good" onPress={() => {}}>
+                        <Icon name="heart" style={styles.actionButtonIcon} />
+                    </ActionButton.Item>                    
+                </ActionButton>
             </Container>
         )
     }
