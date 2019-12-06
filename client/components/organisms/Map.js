a678d0534ca65bdb27cecc325135dc116d6dcdec
FukeKazki <kazkichi0906@gmail.com>
2019-12-07 01:01:41 +0900

    Update: マーカーの表示 propsで位置情報とマーカ配列を流したり、リファクタしたり。

--- client/components/organisms/Map.js
+++ client/components/organisms/Map.js
@@ -1,17 +1,22 @@
 import React from 'react'
-import MapView from 'react-native-maps'
+import MapView, { Marker } from 'react-native-maps'
 
-const Map = () => {
+const Map = ({ region, markers }) => {
     return (
         <MapView
             style={{ flex: 1 }}
-            initialRegion={{
-                latitude: 37.78825,
-                longitude: -122.4324,
-                latitudeDelta: 0.0922,
-                longitudeDelta: 0.0421
-            }}
-        />
+            region={region}
+        >
+            {markers.map((marker, idx) => (
+                <Marker
+                    key={idx}
+                    coordinate={{
+                        latitude: marker.latitude,
+                        longitude: marker.longitude,
+                    }}
+                />
+            ))}
+        </MapView>
     )
 }
 
