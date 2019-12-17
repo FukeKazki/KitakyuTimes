f04a4dcc406ee26110d5f5018fc38eec67741a75
FukeKazki <kazkichi0906@gmail.com>
2019-12-18 02:11:10 +0900

    Add: body-parserの追加

--- server/node/index.js
+++ server/node/index.js
@@ -14,6 +14,7 @@ app.use((req, res, next) => {
 app.use(bodyParser.urlencoded({
     extended: true
 }))
+app.use(bodyParser.json())
 /* ========================= */
 // テスト
 app.get('/', (req, res) => {
