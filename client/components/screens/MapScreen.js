6f09d131625580d158ee16de93af75ad93c6862b
FukeKazki <kazkichi0906@gmail.com>
2019-12-18 02:17:02 +0900

    Update: いろいろたくさん追加しました。 なぜ細かくコミットしてないかって？ そんな余裕。なかったんだよ。 許してくれ。

--- client/components/screens/MapScreen.js
+++ client/components/screens/MapScreen.js
@@ -6,6 +6,9 @@ import Header from '../organisms/Header'
 import Fab from '../organisms/ActionButton'
 import * as Location from 'expo-location'
 import * as Permissions from 'expo-permissions'
+import axios from 'axios'
+
+axios.defaults.baseURL = 'https://31aff36a.ngrok.io/api/v1'
 
 export default class Map extends React.Component {
     constructor(props) {
@@ -18,15 +21,39 @@ export default class Map extends React.Component {
                 latitudeDelta: 0.2,
                 longitudeDelta: 0.2,
             },
-            markers: [
-                { latitude: 33.777, longitude: 130.60, type: "cat" },
-                { latitude: 33.7048298, longitude: 130.8184429, type: "dog" },
+            markers: [],
+            posts: [
+                { post_id: 1, post_user_id: 1, post_content: { title: '2019秋企画展　いろ色ひろがる印刷展', description: '「いろ色ひろがる印刷展」では、現代の印刷技術を中心に取り上げ、印刷の歴史、工場での印刷、多種多様な色と紙から生まれるアートとしてのポスターも展示します。見るだけでも楽しい色見本や不思議な見え方の特殊印刷、手作り感が今なお人気の活版印刷をとりあげるコーナーも設け、幅広い印刷技術についてご紹介します。印刷技術の応用でこれから生活や学び方法の変化、そして記録媒体としての「印刷」が担う文化継承の一端に実際に触れ、体験していただきます。' }, post_geo: [33.8689236, 130.8056444], post_date: '2019-12-26' },
+                { post_id: 1, post_user_id: 1, post_content: { title: 'アトリエ ハーモニー　作品展', description: '木の実やスパイスを使ったリースやオーナメントの展示販売のほか、ドアベルや香り袋の手作り体験もできるイベント「アトリエ ハーモニー 作品展」を開催します。' }, post_geo: [33.9057166, 130.9313072], post_date: '2019-12-26' },
+                { post_id: 1, post_user_id: 1, post_content: { title: 'KPF（北九州ポップカルチャーフェスティバル）2019', description: 'アニメ・マンガなどポップカルチャーをテーマにした九州最大級のイベント。声優トークショー、アニソンライブ、アニメ・マンガ作品展示、クリエイティブ体験、コスプレイベント、痛車展示など、内容盛りだくさんです。' }, post_geo: [33.889573, 130.8830893], post_date: '2019-12-26' },
             ],
         }
     }
-
     componentWillMount() {
         this._getLocationAsync()
+    }
+    componentDidMount() {
+        this._getLocationList()
+    }
+    postLocation = async (type) => {
+        await axios.post('/post/location', {
+            data: {
+                location_user_id: 1,
+                location_date: new Date().toLocaleString(),
+                location_content: {
+                    type: type,
+                },
+            },
+            lat: this.state.region.latitude,
+            lang: this.state.region.longitude,
+        })
+        this._getLocationList()
+    }
+    _getLocationList = async () => {
+        const res = await axios.get('/post/location_list')
+        this.setState({
+            markers: res.data
+        })
     }
     // 位置情報の取得
     _getLocationAsync = async () => {
@@ -52,9 +79,15 @@ export default class Map extends React.Component {
         const { dispatch } = this.props.navigation
         return (
             <Container>
-                <Header dispatch={() => dispatch(DrawerActions.toggleDrawer())} header_title="MapScreen" />
-                <MapView region={this.state.region} markers={this.state.markers} />
-                <Fab />
+                <Header dispatch={() => dispatch(DrawerActions.toggleDrawer())} header_title="KitakyuTimes" />
+                <MapView
+                    region={this.state.region}
+                    markers={this.state.markers}
+                    posts={this.state.posts}
+                />
+                <Fab
+                    postLocation={(type) => this.postLocation(type)}
+                />
             </Container>
         )
     }
